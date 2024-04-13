5838e968d80acf2cc7ba8b15fe5adfd0602a2076
Théo Phan <E238665T@etu.univ-nantes.fr>
2024-04-13 22:59:21 +0200

    ok fix problem

---
+++ src/models/Character.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import characterScene from '../assets/3d/skelleton_character.glb'
+import { useGLTF } from '@react-three/drei'
+const Character = ({ isRotating, ...props }) => {
+    const  {scene, animations} = useGLTF(characterScene)
+    return (
+        <mesh {...props}>
+            <primitive object={scene} />
+        </mesh>
+    )
+}
+
+export default Character;
