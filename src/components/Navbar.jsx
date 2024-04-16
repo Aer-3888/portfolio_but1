731586701e6d02c2192c23fe6bf51e43000770e5
Théo Phan <E238665T@etu.univ-nantes.fr>
2024-04-16 23:04:28 +0200

    bar and link project and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,14 @@ const Navbar = () => {
         <NavLink to="/" className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
             <p >Accueil</p>
         </NavLink>
-        
+        <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/AboutMe' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <a href='../AboutMe#project' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </a>
+      </nav>
     </header>
   )
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import Laptop from '../models/Laptop'
+import Loader from '../components/Loader';
 
 const AboutMe = () => {
   return (
@@ -10,23 +13,49 @@ const AboutMe = () => {
         <p>
         Je m'appelle Théo PHAN et je suis actuellement en première année d'un BUT en Informatique à l'IUT de Nantes. Passionné par les défis du monde numérique, j'ai choisi cette filière pour explorer la programmation et le fonctionnement des technologies. Mon objectif académique est d'intégrer une école d'ingénieur à la fin de ma deuxième année. Cette étape me permettra d'approfondir mes connaissances techniques et sera un tremplin pour ma future carrière professionnelle. En ce qui concerne mes objectifs professionnels, je suis encore en phase d'exploration. Cependant, je suis animé par une curiosité pour les différents domaines de l'informatique, tels que le développement logiciel, la cybersécurité ou encore l'intelligence artificielle.
         </p>
+        
       </div>
       <div>
         <span  className='head-text'>Compétences</span>
         <div className='flex'>
           <div className='w-1/2 m-1'>
             <div >Python</div>
-            <div className='h-6 w-1/2' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
-              <div className='w-1/2 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
+            <div className='h-6 w-3/5' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
+              <div className='w-3/4 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
             </div>
           </div>
           <div className='w-1/2 m-1'>
-          <div >Python</div>
-            <div className='h-6 w-1/2' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
+            <div >Kotlin/Java</div>
+            <div className='h-6 w-3/5' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
+              <div className='w-4/6 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
+            </div>
+          </div>
+        </div>
+        <div className='flex'>
+          <div className='w-1/2 m-1'>
+            <div >Html/Css</div>
+            <div className='h-6 w-3/5' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
+              <div className='w-3/5 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
+            </div>
+          </div>
+          <div className='w-1/2 m-1'>
+            <div >JavaScript</div>
+            <div className='h-6 w-3/5' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
               <div className='w-1/2 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
             </div>
           </div>
         </div>
+        <div className='flex'>
+          <div className='w-1/2 m-1'>
+            <div >Golang</div>
+            <div className='h-6 w-3/5' style={{borderColor: 'black', borderWidth : 1, borderRadius : 7}}>
+              <div className='w-3/4 h-4 m-1 rounded' style={{backgroundColor : 'black'}}></div>
+            </div>
+          </div>
+        </div>
+        <span className='head-text w-full' id='project'>Mes projets</span>
+        
+
       </div>
       
     </section>
