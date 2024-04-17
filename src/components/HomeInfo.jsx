b2a18471bdb0f640695ba2f16dc31f0834c2331d
Théo Phan <E238665T@etu.univ-nantes.fr>
2024-04-17 22:01:35 +0200

    pls

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -18,7 +18,7 @@ const HomeInfo = ({ currentStage, canvasExpansion }) => {
                 <p className='font-medium sm:text-xl text-center'>
                     Je suis un étudiant en première année d&#8217;un BUT en Informatique.Vous trouverez ci-dessous mon CV.
                 </p>
-                <a href={getPDFUrl("CV_PHAN_Theo.pdf")}  className='neo-brutalism-white neo-btn'>
+                <a href={getPDFUrl("CV_PHAN_Theo.pdf")} download={'CV_PHAN_Theo.pdf'} className='neo-brutalism-white neo-btn'>
                     Mon CV
                     <img src={download} alt='download' className='w-4 h-4 object-contain' />
                 </a>
@@ -28,7 +28,7 @@ const HomeInfo = ({ currentStage, canvasExpansion }) => {
                 <p className='font-medium sm:text-xl text-center'>
                 Et ci-dessous, vous trouverez ma lettre de motivation, où je partage mon enthousiasme, mes compétences et ma passion pour cette opportunité.
                 </p>
-                <a href={getPDFUrl("Lettre_de_motivation_PHAN_Theo.pdf")} className='neo-brutalism-white neo-btn'>
+                <a href={getPDFUrl("Lettre_de_motivation_PHAN_Theo.pdf")} download='Lettre_de_motivation_PHAN_Theo.pdf' className='neo-brutalism-white neo-btn'>
                     Ma lettre de motivation
                     <img src={download} alt='download' className='w-4 h-4 object-contain'/>
                 </a>
